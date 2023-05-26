957f915835f4c6ff8b556bb7b87c1702fe7ab69a
Sven Olding <sven.olding@gmx.de>
2023-05-26 13:26:19 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# PatternMatching
+
+Sample from the YT Video [Pattern Matching in Elixir](https://youtu.be/kjRNvTVhc9g) form [Backend Stuff](https://www.youtube.com/@backendstuff)
+
+### Exercise Solution by BackendStuff
+
+https://github.com/BackendStuff/elixir_basics_solutions
---
+++ lib/saucer_preflight.ex
@@ -0,0 +1,11 @@
+defmodule SaucerPreflight do
+  defp max_flying_load_lbs, do: 55
+  defp convert_kg_to_lb(kg_value), do: kg_value * 2.2
+
+  # write a recursive loop that will convert kg to lbs
+  # elem(0) -> get element from tuple
+
+  def is_under_max_load?(list) do
+    # call the recursive function
+  end
+end
