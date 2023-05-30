dfa3dc09ce608260db3ec6c760fbff54567058f6
Sven Olding <sven.olding@gmx.de>
2023-05-30 16:42:06 +0200

    solve exercise

--- lib/saucer_preflight.ex
+++ lib/saucer_preflight.ex
@@ -1,11 +1,14 @@
 defmodule SaucerPreflight do
+  def is_under_max_load?(list) do
+    sum =
+      list
+      |> Enum.map(&EquipmentDetails.item_details(&1))
+      |> Enum.map(fn e -> convert_kg_to_lb(elem(e, 0)) * elem(e, 2) end)
+      |> Enum.sum()
+
+    sum <= max_flying_load_lbs()
+  end
+
   defp max_flying_load_lbs, do: 55
   defp convert_kg_to_lb(kg_value), do: kg_value * 2.2
-
-  # write a recursive loop that will convert kg to lbs
-  # elem(0) -> get element from tuple
-
-  def is_under_max_load?(list) do
-    # call the recursive function
-  end
 end
